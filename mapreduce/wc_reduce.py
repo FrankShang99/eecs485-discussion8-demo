33470b10e75116a2d2ffc5b34b4527421c9f0670
Nick Smart <npsmart@umich.edu>
2019-03-12 12:01:30 -0400

    Updated reduce to use collections defaultdict

--- mapreduce/wc_reduce.py
+++ mapreduce/wc_reduce.py
@@ -1,15 +1,11 @@
+import collections
 import sys
 
-words = {}
+words = collections.defaultdict(int)
 # Append all the words in the input files to dictionary of word-count pairs
 for line in sys.stdin:
     split_line = line.rsplit()
-    word = split_line[0]
-    count = int(split_line[1])
-    if word in words:
-        # TODO: IMPLEMENT ME
-    else:
-        # TODO: IMPLEMENT ME
+    # TODO: IMPLEMENT ME
 
 for key, val in words.items():
     # TODO: PRINT KEY-VALUE PAIRS
